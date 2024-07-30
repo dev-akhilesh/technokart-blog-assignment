a9cd7673d6ee15855f6cae68fead986558e2130d
dev-akhilesh <akhileshtakawale703@gmail.com>
2024-07-30 20:39:58 +0530

    completed

--- client/src/components/account/Login.jsx
+++ client/src/components/account/Login.jsx
@@ -80,7 +80,7 @@ const Login = ({ isUserAuthenticated }) => {
     const navigate = useNavigate();
     const { setAccount } = useContext(DataContext);
 
-    const imageURL = 'https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png';
+    const imageURL = 'http://bookboon.com/blog/wp-content/uploads/2013/06/blog-1.jpg';
 
     useEffect(() => {
         showError(false);
